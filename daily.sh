b6378d66fed518bd73fe469f77bbea3164131be2
Alexander <alex@ws54.tk>
2016-10-19 22:55:06 +0700

    New changes

--- daily.sh
+++ daily.sh
@@ -1,6 +1,6 @@
 cd ~/dailypush/
 echo -n "." >> README.md
-sed -i -re 's/(\.{80})\./\1\n./g' README.md
+sed -i -re 's/(\.{200})\./\1\n./g' README.md
 git add *
 git commit -m 'New changes'
 git push origin master
