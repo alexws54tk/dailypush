74f05e5e0507941d1d0c2e5ec684631e2b5144ce
Alexander <alex@ws54.tk>
2016-11-15 13:08:22 +0700

    New changes

--- daily.sh
+++ daily.sh
@@ -4,3 +4,4 @@ sed -i -re 's/^(\.{199})\./\1\n./g' README.md
 git add *
 git commit -m 'New changes'
 git push origin master
+wget -qO- http://radiopleer.com/info/nashe_last_tracks.txt | sed -e 's/\r//g' -e 's,</li>,,g' -re 's,^.{11},,g' >> ~/nashe_last_tracks.txt
