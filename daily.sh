972ee0c597370846d9a013584e74e9479f0d56f8
Alexander <alex@ws54.tk>
2022-11-20 22:06:57 +0700

    New changes 20221120 2206

--- daily.sh
+++ daily.sh
@@ -2,5 +2,5 @@ cd ~/dailypush/
 echo -n "." >> README.md
 sed -i -re 's/^(\.{144})\./\1\n./g' README.md
 git add *
-git commit -m "New changes $(date +"%y%m%d %H%M")"
+git commit -m "New changes $(date +"%Y%m%d %H%M")"
 git push origin master
